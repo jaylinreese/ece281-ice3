8fa98df10f715ec6e0a46da724f05c36f6ad9e1c
Reese <GITHUB_NOREPLY_EMAIL@users.noreply.github.com>
2026-02-13 16:12:21 -0700

    aaa

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -55,7 +55,7 @@ begin
 	       assert (w_sum = x"8" and w_Cout = '0') report "bad with ones 3" severity failure;
        w_addends <= x"1F"; w_Cin <= '0'; wait for 10 ns;
 	       assert (w_sum = x"0" and w_Cout = '1') report "bad with ones 4" severity failure;	
-       w_addends <= x"53"; w_Cin <= '0'; wait for 10 ns;
+       w_addends <= x"54"; w_Cin <= '0'; wait for 10 ns;
 	       assert (w_sum = x"9" and w_Cout = '0') report "bad with ones 5" severity failure;		
        w_addends <= x"96"; w_Cin <= '0'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '0') report "bad with ones 6" severity failure;		
